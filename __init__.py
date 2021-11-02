611ace496df097dcd2d3054c532c5e0953e94b8e
Bernadette Mohr <b.j.mohr@uva.nl>
2021-11-02 14:12:03 +0100

    added __init__.py and straightened out directory structure.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .AutoFE import graph2topology
+from .AutoFE import graph2coordinates
+from .AutoFE import minimize_graphs
+from .AutoFE import equilibrate_charge
+from .AutoFE import copy_files
+from .AutoFE import system_setup
+from .AutoFE import windows
